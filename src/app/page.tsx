375d0efcfa5258b7b4cea203c402216214c20189
Valeriu Mazur <160285130+VALGFX@users.noreply.github.com>
2025-06-09 12:28:29 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import ProductItem from "@/components/ProductItem";
 import Latest from "@/components/Latest";
 import BestSeller from "@/components/Bestseller";
 
 export default function Home() {
   return (
-      <>
-        <Navbar />
-        <main>
-          <Hero/>
-            <Latest/>
-            <BestSeller/>
-        </main>
-      </>
-  )
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <Latest />
+        <BestSeller />
+      </main>
+    </>
+  );
 }
